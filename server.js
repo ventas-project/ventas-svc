61d1c1e01c330865da0828921a160016a6343d10
user <user@users-iMac.local>
2017-10-16 15:42:23 +0900

    pm start scipt add

---
+++ server.js
@@ -0,0 +1,9 @@
+/*
+pm2 를 이용해서 서버를 시작시킨다.
+pm2 start server -- start ventas-insight
+*/
+
+//var bitcore = require('../bitcore-node/lib/cli/bitcore');
+//bitcire();
+var bitcore = require('bitcore-node');
+bitcore.cli.bitcore();
